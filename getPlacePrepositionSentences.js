0757317415e74c6fd865344f8fe14e47f36c3b28
Damir Rekic <rdamir82@gmail.com>
2020-09-20 11:25:14 +0200

    Added new sentences

--- getPlacePrepositionSentences.js
+++ getPlacePrepositionSentences.js
@@ -1,12 +1,17 @@
 function getPlacePrepositionsSentences() {
     return [
-      ["The remote control is __ the table.", '', 'on', 'on top of'],
-      ["Look at the clock __ the mantelpiece.", '', 'above'],
-      ["The keys are __ the mat.", '', 'under', 'underneath'],
-      ["The chart __ shows the annual figures.", '', 'below' ],
-      ["It was __ freezing last night.", '', 'below' ],
-      ["A is __ to B in the alphabet.", '', 'next' ],
+      ["The remote control is __ the table.", 'touches a surface', 'on', 'on top of'],
+      ["Look at the clock __ the mantelpiece.", 'higher than sth.', 'above'],
+      ["The keys are __ the mat.", 'below sth.', 'under', 'underneath'],
+      ["The chart __ shows the annual figures.", 'lower than', 'below' ],
+      ["It was __ freezing last night.", 'lower than', 'below' ],
+      ["A is __ to B in the alphabet.", 'beside', 'next' ],
       ["B is __ A and C in the alphabet.", '', 'between' ],
-      ["Walk __ the road, to the other side.", '', 'across']
+      ["Walk __ the road, to the other side.", 'from one side to the other side', 'across'],
+	  ["The picture hangs __ my bed.", 'higher than sth.', 'above'],
+	  ["There isn't a bridge __ the river.", 'from one side to the other side', 'across'],
+	  ["The dog ran __ the cat.", 'one follows the other', 'after'],
+	  ["The cat is __ the table." "below sth.", "under"]
+	  
     ];
   }
